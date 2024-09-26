6edb7dcae61e61d58427bf2f731960e93775a04a
ETATUNG\Chenting.Weng <Chenting.Weng@etatung.com>
2024-09-26 12:02:36 +0800

    增加 gRPC 的 Server Demo。

---
+++ DemoGrpcService/Services/DemoService.cs
@@ -0,0 +1,18 @@
+﻿using DemoGrpcServiceDemo;
+using Grpc.Core;
+
+namespace DemoGrpcService.Services
+{
+    public class DemoService : Demo.DemoBase
+    {
+        public override Task<HelloReply> PrintTime(HelloRequest request, ServerCallContext context)
+        {
+            var age = request.Age;
+            var address = request.Address;
+            string message = $"{DateTime.Now:HH:mm:ss.fffzzz} - Name = {request.Name}";
+            Console.WriteLine(message);
+            var result = new HelloReply { Message = message };
+            return Task.FromResult(result);
+        }
+    }
+}
