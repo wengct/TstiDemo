287add2fc3d3eb731614f5efcf577c4b1b82fb4b
ETATUNG\Chenting.Weng <Chenting.Weng@etatung.com>
2024-10-17 10:28:51 +0800

    新增 grpc client

---
+++ DemoGrpcClient/Program.cs
@@ -0,0 +1,26 @@
+﻿using DemoGrpcServiceDemo;
+using Grpc.Net.Client;
+
+namespace DemoGrpcClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (Console.ReadLine() != "z")
+            {
+                using var channel = GrpcChannel.ForAddress("https://localhost:7052");
+                Demo.DemoClient client = new Demo.DemoClient(channel);
+                HelloRequest helloRequest = new HelloRequest()
+                {
+                    Name = Guid.NewGuid().ToString(),
+                    Address = "localhost",
+                    Age = 18
+                };
+                HelloReply response = client.PrintTime(helloRequest);
+
+                Console.WriteLine(response.Message);
+            }
+        }
+    }
+}
